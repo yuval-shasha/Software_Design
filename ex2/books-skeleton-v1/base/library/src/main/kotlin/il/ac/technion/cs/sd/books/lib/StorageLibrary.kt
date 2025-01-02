4f7932d11ea7c41be876a632a20d0e8c0a177d88
yuval-shasha <yuvalshasha11@gmail.com>
2025-01-02 21:53:08 +0200

    Adding new implementation to support the new data arrangement. Added helper functions for better readability.

--- ex2/books-skeleton-v1/base/library/src/main/kotlin/il/ac/technion/cs/sd/books/lib/StorageLibrary.kt
+++ ex2/books-skeleton-v1/base/library/src/main/kotlin/il/ac/technion/cs/sd/books/lib/StorageLibrary.kt
@@ -12,42 +12,48 @@ class StorageLibrary(lineStorageFactory: LineStorageFactory, fileName: String)
         lineStorage = lineStorageFactory.open(fileName)
     }
 
-    // return the line in the LineStorage that starts with the provided id
+    // TODO: Mia, go over this function. added this for better readability
+   private fun binarySearchOnOddLines(id: String): String? {
+       var left = 0
+       var right = lineStorage?.numberOfLines()!! / 2
+       while (left <= right) {
+           val mid = (left + right) / 2
+           val currentId = lineStorage?.read(mid * 2)
+           if (currentId == id) {
+               return lineStorage?.read(mid * 2 + 1)
+           }
+          if (currentId != null && currentId < id) {
+            left = mid + 1
+          } else {
+            right = mid - 1
+          }
+       }
+       return null
+   }
+
+    // TODO: Mia, go over this function
+    // return the line in the LineStorage that contains the id's data
     // return null if the id is not found
     private fun searchForIdLineInLineStorage(id: String): String? {
-        var left = 0
-        var right = lineStorage?.numberOfLines()!! - 1
-        while (left <= right) {
-            val mid = (left + right) / 2
-            val currentLine = lineStorage?.read(mid)
-            val currentId = currentLine?.split(",")!![0]
-            if (currentId == id) {
-                return currentLine
-            }
-            if (currentId < id) {
-                left = mid + 1
-            } else {
-                right = mid - 1
-            }
-        }
-        return null
+        return binarySearchOnOddLines(id)
     }
 
-    fun initializeDatabase(reviewerList : List<Reviewer>) {
-        reviewerList
-            .asSequence()
+    private fun Sequence<Reviewer>.mergeAllReviewsForReviewer() : Sequence<Reviewer> =
+        this
             .groupBy { it.id }
             .asSequence()
             // concatenating the review of the same reviewer
             .map { (id, reviewersWithSameId) ->
-                    val concatenatedReviews = reviewersWithSameId
-                        .asSequence()
-                        .flatMap { it.reviews.asSequence() }
-                        .toList()
+                val concatenatedReviews = reviewersWithSameId
+                    .asSequence()
+                    .flatMap { it.reviews.asSequence() }
+                    .toList()
 
-                    Reviewer(id, concatenatedReviews)
+                Reviewer(id, concatenatedReviews)
             }
-            // removing duplicates of books with the same name
+
+    private fun Sequence<Reviewer>.removeDuplicateBooks() : Sequence<Reviewer> =
+        this
             .map { reviewer ->
                 val uniqueBooksList = reviewer.reviews
                     .asSequence()
@@ -57,29 +63,51 @@ class StorageLibrary(lineStorageFactory: LineStorageFactory, fileName: String)
 
                 Reviewer(reviewer.id, uniqueBooksList)
             }
-            .sortedBy {  it.id }
-            // sorting each book list by name
+
+    private fun Sequence<Reviewer>.sortEachBookListByName() : Sequence<Reviewer> =
+        this
             .map { reviewer ->
                 val sorted = reviewer.reviews
                     .sortedBy { it.name }
                 Reviewer(reviewer.id, sorted)
             }
+
+    // TODO: check correctness
+    fun initializeDatabase(reviewerList : List<Reviewer>) {
+        reviewerList.asSequence()
+            .mergeAllReviewsForReviewer()
+            .removeDuplicateBooks()
+            // sorting the reviewer list by id
+            .sortedBy {  it.id }
+            .sortEachBookListByName()
             .toList()
 
 
             for (reviewer in reviewerList)
             {
-                lineStorage.appendLine(reviewer.id)
-                lineStorage.appendLine(reviewer.reviews.joinToString(" ") { "${it.name} ${it.score}" })
+                // returns null if lineStorage is null
+                // if lineStorage is null, these calls will do nothing
+                lineStorage?.appendLine(reviewer.id)
+                lineStorage?.appendLine(reviewer.reviews.joinToString(" ") { "${it.name} ${it.score}" })
             }
     }
 
-    fun getDataFromId(id: String): String? {
-        val lineOfId = searchForIdLineInLineStorage(id) ?: return null
-        val dataOfId = lineOfId.split(",")[1]
+
+    // TODO: Mia, go over this function. changed the return type to Map<String, String>
+    // data is stored in the following format:
+    // <id1> <data1> <id2> <data2> ...
+    fun getDataFromId(id: String): Map<String,String>? {
+        val lineOfData = searchForIdLineInLineStorage(id) ?: return null
+        val dataOfId = lineOfData
+            .split(" ")
+            .asSequence()
+            .chunked(2)
+            .associate { it[0] to it[1] }
+            .toMap()
         return dataOfId
     }
 
+    // TODO: check if really needed
     fun getDatabaseAsArrayList(): ArrayList<String> {
         val dataList = ArrayList<String>()
         val numberOfLines = lineStorage?.numberOfLines();
